a610734dd3b5b59a0da5a6d2eab0ba7ccbfce167
Senthil Kumar S A <sasenthilk@gmail.com>
2021-11-02 03:23:52 +0530

    Added: Greeting model class to store greeting details

---
+++ src/main/java/com/bridgelabz/greetingappdevelopment/model/Greeting.java
@@ -0,0 +1,34 @@
+package com.bridgelabz.greetingappdevelopment.model;
+
+public class Greeting {
+
+    private long id;
+    private String message;
+
+    public Greeting(long id, String message) {
+
+        this.id = id;
+        this.message = message;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "Greeting [id=" + id + ", message=" + message + "]";
+    }
+}
